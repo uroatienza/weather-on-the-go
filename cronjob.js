39239554eff0fe88f974e6888ecdb214c1e66d4e
Gian Santillan <gian@gorated.ph>
2013-09-01 01:12:44 +0800

    added mine

--- cronjob.js
+++ cronjob.js
@@ -1,6 +1,6 @@
 var cronJob = require('cron').CronJob;
 var http 	= require("http");
 
-new cronJob('* * * * * *', function(){
-    console.log('You will see this message every second');
-}, null, true, "America/Los_Angeles");
+new cronJob('* */03 * * * ', function(){
+    console.log('You will see this message 3 hours');
+}, null, true, "China/Beijing");
