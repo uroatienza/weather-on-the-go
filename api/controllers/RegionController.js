78081e0ab611ebd9b6d73d74a7bb3f28b70d2043
Gian Santillan <gian@gorated.ph>
2013-09-01 02:46:44 +0800

    only run it once - /region/

--- api/controllers/RegionController.js
+++ api/controllers/RegionController.js
@@ -39,10 +39,125 @@ module.exports = {
           }
         },
         {
-          
-        }
+          region_name : "Region 3",
+          region_code : "reg-3",
+          loc : {
+            lat   : "15.482772200000000000",
+            long  : "120.712002300000000000"
+          }
+        },
+        {
+          region_name : "Region 4",
+          region_code : "reg-4",
+          loc : {
+            lat   : "14.1007803",
+            long  : "121.07937049999998" 
+          }
+        },
+        {
+          region_name : "Region 5",
+          region_code : "reg-5",
+          loc : {
+            lat   : "13.4209885",
+            long  : "123.41367360000004",
+          }
+        },
+        {
+          region_name : "Region 6",
+          region_code : "reg-6",
+          loc : {
+            lat   : "11.0049836",
+            long  : "122.53727409999999"
+          }
+        },
+        {
+          region_name : "Region 7",
+          region_code : "reg-7",
+          loc : {
+            lat   : "10.2968562",
+            long  : "123.8886774"
+          }
+        },
+        {
+          region_name : "Region 8",
+          region_code : "reg-8",
+          loc : {
+            lat   : "12.2445533",
+            long  : "125.03881639999997"
+          }
+        },
+        {
+          region_name : "Region 9",
+          region_code : "reg-9",
+          loc : {
+            lat   : "8.154077",
+            long  : "123.25879299999997"
+          }
+        },
+        {
+          region_name : "Region 10",
+          region_code : "reg-10",
+          loc : {
+            lat   : "8.020163499999999",
+            long  : "124.68565089999993"
+          }
+        },
+        {
+          region_name : "Region 11",
+          region_code : "reg-11",
+          loc : {
+            lat   : "7.3041622",
+            long  : "126.08934060000001"
+          }
+        },
+        {
+          region_name : "Region 12",
+          region_code : "reg-12",
+          loc : {
+            lat   : "6.270691800000000000",
+            long  : "124.685650899999930000" 
+          }
+        },
+        {
+          region_name : "Region 13",
+          region_code : "reg-13",
+          loc : {
+            lat   : "8.801456199999999",
+            long  : "125.74068820000002"
+          }
+        },
+          {
+            region_name : "ARMM",
+            region_code : "armm",
+            loc : {
+              lat   : "6.956831299999999",
+              long  : "124.2421597"
+            }
+          },
+          {
+            region_name : "CAR",
+            region_code : "car",
+            loc : {
+              lat   : "17.3512542",
+              long  : "121.17188510000005"
+            }
+          },
+          {
+            region_name : "NCR",
+            region_code : "ncr",
+            loc : {
+              lat : "14.609053700000000000",
+              long : "121.022256500000030000"
+            }
+          }
     ];
 
+
+    for(var i = 0; i < regions.length; i++) {
+      Region.create(regions[i]).done(function(err, region) {
+        console.log("done");
+      });
+    }
   }
 
 };
