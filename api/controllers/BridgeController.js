4787ffe1ebd83a431692e75fb50a6f9091957c8e
Gian Santillan <gian@gorated.ph>
2013-08-31 15:41:48 +0800

    init

---
+++ api/controllers/BridgeController.js
@@ -0,0 +1,39 @@
+/**
+ * BridgeController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+  /* e.g.
+  sayHello: function (req, res) {
+    res.send('hello world!');
+  }
+  */
+  
+  /**
+   * /bridge/login
+   */ 
+  login: function (req,res) {
+
+    // This will render the view: 
+    // /home/gian/weather-on-the-go/views/bridge/login.ejs
+    res.view();
+
+  },
+
+
+  /**
+   * /bridge/logout
+   */ 
+  logout: function (req,res) {
+
+    // This will render the view: 
+    // /home/gian/weather-on-the-go/views/bridge/logout.ejs
+    res.view();
+
+  }
+
+};
