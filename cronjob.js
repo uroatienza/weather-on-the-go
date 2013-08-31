e4567fda5aa7cabc37f727a73c584713ef784bfc
Gian Santillan <gian@gorated.ph>
2013-09-01 02:59:28 +0800

    added data

--- cronjob.js
+++ cronjob.js
@@ -1,6 +1,26 @@
 var cronJob = require('cron').CronJob;
 var http 	= require("http");
 
-new cronJob('* */03 * * * ', function(){
-    console.log('You will see this message 3 hours');
+//new cronJob('* */03 * * * ', function(){
+new cronJob('* * * * * *', function() {
+   var options = {
+   		host : "localhost:1337",
+   		port : 80,
+   		path : "/api/forcast",
+   		method : "GET"
+   };
+
+   var request = http.get(options, function(res) {
+   		console.log('Data initiated');
+   		res.on('data', function(chunk) {
+   			console.log("added data "+chunk);
+   		});
+
+   		res.on('end',function() {
+   			console.log("data ended..");
+   		});
+   });
+
+
+
 }, null, true, "China/Beijing");
